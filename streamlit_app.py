47230ef5284dda0b58102752cc4b141fb6702e8d
pritishnair85 <91715971+pritishnair85@users.noreply.github.com>
2022-12-14 15:02:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # response extraction in json
 # streamlit.text(fruityvice_response.json())
 
