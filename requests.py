ce2323ef30fb7e0fdc2348fa01bda48afaf6215e
pritishnair85 <91715971+pritishnair85@users.noreply.github.com>
2022-12-14 14:53:22 +0530

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,5 +1,6 @@
+import requests
+
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
